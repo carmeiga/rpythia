f5bf9b35957a331cf12fcc9ff0678095f1f76def
Carlos M. García <64075606+carmeiga@users.noreply.github.com>
2021-02-15 13:01:36 +0100

    c++ minkowski metric

--- script.r
+++ script.r
@@ -3,6 +3,23 @@ h=mesmo_proceso('h',1)
 w=mesmo_proceso('w',2)
 t=mesmo_proceso('t',3)
 
+library(transport)
+
+## user-defined distance function
+library(RcppArmadillo)
+# Use RcppXPtrUtils for simple usage of C++ external pointers
+library(RcppXPtrUtils)
+library(parallelDist)
+## user-defined distance function
+minkowskiFuncPtr <- cppXPtr(
+  "double customDist(const arma::mat &A, const arma::mat &B) {
+  return (arma::accu(arma::square(A - B))-2*pow(A(0,0)-B(0,0),2));
+}", depends = c("RcppArmadillo"))
+
+
+# distance matrix for user-defined euclidean distance function (note that method is set to "custom")
+
+
 
 
 estables=rbind(s,h,w,t)
@@ -16,11 +33,11 @@ for(i in 1:nev) {
 aggregate(estables$en, list(estables$ev), sum)
 
 d=matrix(0,nrow=nev,ncol=nev)
-for(k in 13:nev) {
+for(k in 1:nev) {
   for(p in 1:nev) {
 
-proba1=estables[estables$ev==p,c((11:15),22)]
-proba2=estables[estables$ev==k,c((11:15),22)]
+proba1=estables[estables$ev==p,c(22,14,(11:13))]
+proba2=estables[estables$ev==k,c(22,14,(11:13))]
 
 
 
@@ -28,16 +45,26 @@ n=nrow(proba1)
 m=nrow(proba2)
 custos=matrix(0,nrow=nrow(proba1),ncol=nrow(proba2))
 
-minkowski<-function(cuadri1,cuadri2) {
-  return(-cuadri1[4]*cuadri1[4]+sum(cuadri1[1:3]*cuadri1[1:3])-cuadri2[4]*cuadri2[4]+sum(cuadri2[1:3]*cuadri2[1:3])-2*(-cuadri1[4]*cuadri2[4]+sum(cuadri1[1:3]*cuadri2[1:3])))
-}
 
-for(i in 1:n) {
-  for(j in 1:m){
-    
-    custos[i,j]=minkowski(as.numeric(proba1[i,]),as.numeric(proba2[j,]))
-}
-}
+# minkowski<-function(cuadri1,cuadri2) {
+#   return(-cuadri1[1]*cuadri1[1]+sum(cuadri1[2:4]*cuadri1[2:4])-cuadri2[1]*cuadri2[1]+sum(cuadri2[2:4]*cuadri2[2:4])-2*(-cuadri1[1]*cuadri2[1]+sum(cuadri1[2:4]*cuadri2[2:4])))
+# }
+# 
+# for(i in 1:n) {
+#   for(j in 1:m){
+#     
+#     custos[i,j]=minkowski(as.numeric(proba1[i,]),as.numeric(proba2[j,]))
+# }
+# }
+
+res=parDist(as.matrix(rbind(proba1[,-1],proba2[,-1])), method="custom", func = minkowskiFuncPtr)
+# }
+
+castres=as.matrix(res)
+
+
+
+custos=t(castres[-seq(1,n),-seq(n+1,m+n)])
 
 
 d[k,p]=sqrt(wasserstein(proba1$en, proba2$en, p=2, tplan=NULL, costm=abs(custos),prob=TRUE)) # 4-Wasserstein
@@ -47,9 +74,9 @@ d[k,p]=sqrt(wasserstein(proba1$en, proba2$en, p=2, tplan=NULL, costm=abs(custos)
 }
   
 
-write.table(d,file="distancias.dat",row.names=FALSE,col.names=FALSE) 
 
-distancias <- read.table("~/particarlos/pythia8303/rpythia/distancias.dat", quote="\"", comment.char="")
+write.table(d,file="distancias.dat",row.names=FALSE,col.names=FALSE)
+distancias <- read.table("distancias.dat", quote="\"", comment.char="")
 
 sim=as.matrix(distancias+t(distancias))/2
 
@@ -73,6 +100,10 @@ library(ggplot2)
 ggplot(estables, aes(name)) + geom_bar()
 
   plot(density(estables$pt[(abs(estables$pt)<2)& (estables$name=='mu+' | estables$name=='mu-')]))
+
+
+
+
 
 
 
